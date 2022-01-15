266cd76f542135b8924afdf8ffc5b0cc2ce03161
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-01-15 13:36:02 +0100

    Added Vector2 reference

---
+++ ScriptableObjects/DataTypes/Vector2Variable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Spounka.Core.DataTypes
+{
+    [CreateAssetMenu(fileName = "Vector2Variable", menuName = "Variables/Vector2")]
+    public class Vector2Variable : Variable<Vector2>
+    {
+    }
+}
