a62c2ed0d3b5e15b9261ee0473aa5a7a4bd3e18b
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-01-17 01:06:32 +0100

    Added BoolInitializer

---
+++ BoolInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Spounka.Core.DataTypes;
+using UnityEngine;
+
+namespace Spounka.Core
+{
+    public class BoolInitializer : MonoBehaviour
+    {
+        [SerializeField] private bool initialValue;
+        [SerializeField] private VariableReference<bool> variable;
+
+        private void Awake()
+        {
+            variable.Value = initialValue;
+        }
+    }
+}
