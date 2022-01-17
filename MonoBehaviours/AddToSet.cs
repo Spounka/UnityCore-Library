09f4a13f5e094dd597ed0692d4bb6e9da55fe159
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-01-17 11:05:47 +0100

    Added "AddToSet" to library + namespace adjusment

---
+++ MonoBehaviours/AddToSet.cs
@@ -0,0 +1,23 @@
+﻿using Spounka.Core.Sets;
+using UnityEngine;
+
+namespace Spounka.core
+{
+    public class AddToSet : MonoBehaviour
+    {
+        [SerializeField] private ObjectSet fishes;
+
+
+        private void OnEnable()
+        {
+            if (fishes != null)
+                fishes.AddElement(gameObject);
+        }
+
+        private void OnDisable()
+        {
+            if (fishes != null)
+                fishes.RemoveElement(gameObject);
+        }
+    }
+}
